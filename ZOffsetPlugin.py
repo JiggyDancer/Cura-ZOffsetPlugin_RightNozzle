90aae83c918584537ff35c0de71322026bec296d
JiggyDancer <67120186+JiggyDancer@users.noreply.github.com>
2026-06-15 13:59:45 -0400

    Update ZOffsetPlugin.py

--- ZOffsetPlugin.py
+++ ZOffsetPlugin.py
@@ -99,6 +99,8 @@ class ZOffsetPlugin(Extension):
 
 
     def _filterGcode(self, output_device):
+        #  Logger.log("d", "**************************************************")
+        # Logger.log("d", "Running _filterGcode")
         scene = self._application.getController().getScene()
 
         global_container_stack = self._application.getGlobalContainerStack()
@@ -107,13 +109,11 @@ class ZOffsetPlugin(Extension):
 
         # get setting from Cura
         z_offset_value = global_container_stack.getProperty("adhesion_z_offset", "value")
-        if z_offset_value == 0:
-            return
-            
         z_offset_value_right_nozzle = global_container_stack.getProperty("adhesion_z_offset_right_nozzle", "value")
-        if z_offset_value_right_nozzle == 0:
-            return
+        Logger.log("d", "z_offset_value = " + str(z_offset_value))
+        Logger.log("d", "z_offset_value_right_nozzle = " + str(z_offset_value_right_nozzle))
         
+            
         use_extensive_offset = global_container_stack.getProperty("adhesion_z_offset_extensive_processing", "value")
 
         gcode_dict = getattr(scene, "gcode_dict", {})
@@ -122,7 +122,7 @@ class ZOffsetPlugin(Extension):
             return
 
         dict_changed = False
-        z_move_regex = re.compile(r"(G.*\sZ)([0-9]*.[0-9]*)(.*)")
+        z_move_regex = re.compile(r"(G[01].*\sZ)([-+]?[0-9]*\.?[0-9]+)(.*)")
 
         for plate_id in gcode_dict:
             gcode_list = gcode_dict[plate_id]
@@ -141,8 +141,10 @@ class ZOffsetPlugin(Extension):
                     gcode_list.insert(2, ";LAYER:0\n" + chunks[1])
 
                 relative_mode = False
-
+                active_nozzle = 0
+                
                 if not use_extensive_offset:
+                    Logger.log("d", "use_extensive_offset = False")
                     # find the first vertical G0/G1, adjust it and reset the internal coordinate to apply offset to all subsequent moves
                     lines = gcode_list[2].split("\n")
                     for (line_nr, line) in enumerate(lines):
@@ -169,38 +171,52 @@ class ZOffsetPlugin(Extension):
 
                 else:
                     # process all G0/G1 lines and adjust the Z value
+                    # Logger.log("d", "gcode_list length = %s" % str(len(gcode_list)))
                     for n in range(2, len(gcode_list)): # all gcode lists / layers, start at layer 1 = gcode list 2
-                        if gcode_list[n] == "T0":
-                            active_nozzle = 0
-                            Logger.log(gcode_list[n] + " - active_nozzle = 0")
-                        elif gcode_list[n] == "T1":
-                            active_nozzle = 1
-                            Logger.log(gcode_list[n] + " - active_nozzle = 1")
-                            
+                        # Logger.log("d", str(n))
                         lines = gcode_list[n].split("\n")
-                        for (line_nr, line) in enumerate(lines):
-                            if line.startswith("G91"):
+                        # Logger.log("d", "lines length = %s" % str(len(lines)))
+                        
+                        for line_nr, line in enumerate(lines):
+                            if line.startswith("T0"):
+                                # Logger.log("d", str(line_nr) + " : " + line)
+                                # Logger.log("d", line + " active nozzle = 0")
+                                active_nozzle = 0
+                                continue
+                            elif line.startswith("T1"):
+                                # Logger.log("d", str(line_nr) + " : " + line)
+                                # Logger.log("d", line + " active nozzle = 1")
+                                active_nozzle = 1
+                                continue
+                            elif line.startswith("G91"):
+                                # Logger.log("d", str(line_nr) + " : " + line)
+                                # Logger.log("d", "relative_mode = True")
                                 relative_mode = True
                                 continue
                             elif line.startswith("G90"):
+                                # Logger.log("d", str(line_nr) + " : " + line)
+                                # Logger.log("d", "relative_mode = False")
                                 relative_mode = False
                                 continue
                             if relative_mode:
+                                # Logger.log("d", "Skip in relative mode")
                                 continue
 
                             result = z_move_regex.fullmatch(line)
+                                                      
                             if result:
                                 try:
                                     if active_nozzle == 0:
                                         adjusted_z = round(float(result.group(2)) + z_offset_value, 5)
                                     else:
                                         adjusted_z = round(float(result.group(2)) + z_offset_value + z_offset_value_right_nozzle, 5)
-
                                 except ValueError:
                                     Logger.log("e", "Unable to process Z coordinate in line %s", line)
                                     continue
                                 lines[line_nr] = result.group(1) + str(adjusted_z) + result.group(3) + " ;adjusted by z offset"
-                                if active_nozzle == 1: lines[line_nr] = lines[line_nr] + " right nozzle"
+                                if active_nozzle == 1: lines[line_nr] += " right nozzle"
+                                lines[line_nr] += " from " + result.group(2) + " mm"
+                                
                                 gcode_list[n] = "\n".join(lines)
 
                 gcode_list[0] += ";ZOFFSETPROCESSED\n"
@@ -212,3 +228,6 @@ class ZOffsetPlugin(Extension):
 
         if dict_changed:
             setattr(scene, "gcode_dict", gcode_dict)
+        
+        # Logger.log("d", "Ending _filterGcode")
+        # Logger.log("d", "**************************************************")
